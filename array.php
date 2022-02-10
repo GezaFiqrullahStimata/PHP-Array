3ee4b6fe3bd70ac6e975dd3b183de282f38c2530
gezayu <gezayu@gmail.com>
2022-02-10 16:53:48 +0700

    array 1

---
+++ array.php
@@ -0,0 +1,14 @@
+<?php
+//Membuat Array
+
+//Cara 1
+$hari = array("senin","selasa","rabu","kamis","jum'at","sabtu","minggu");
+print_r($hari);
+var_dump($hari);
+
+//Cara 2
+$hari2 = ["senin","selasa","rabu","kamis","jum'at","sabtu"];
+$hari2 =["minggu"];
+print_r($hari2);
+var_dump($hari2);
+?>
