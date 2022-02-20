c76b94bdafda0c347b13401ba08754bf9e32509a
gezayu <gezayu@gmail.com>
2022-02-20 18:41:15 +0700

    Array Bersarang : latihan1

---
+++ Array Bersarang/latihan1.php
@@ -0,0 +1,11 @@
+<?php
+$hari = array('Senin','Selasa','Rabu');
+$bulan = ['Janurai','Februari','Maret'];
+$gabungan = [100,'teks',true];
+
+var_dump($hari);
+echo '<br>';
+print_r($bulan);
+echo '<br>';
+echo $gabungan[0];
+?>
---
+++ array.php
@@ -0,0 +1,8 @@
+<?php
+//Membuat Array
+
+//Cara 1
+$hari = array("senin","selasa","rabu","kamis","jum'at","sabtu","minggu");
+print_r($hari);
+
+?>
